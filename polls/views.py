dd98c0e0a34ed4f4c23f481b7cd471899348638c
caicos <bruletmarc@yahoo.fr>
2020-11-15 16:13:02 +0100

    Création d'un formulaire

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse # uniquement utile pour les vues détails, résults et vote qui n'ont pas la fonction render()
+from django.http import HttpResponse, HttpResponseRedirect # HttpResponse est uniquement utile pour les vues détails, résults et vote qui n'ont pas la fonction render()
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request):
@@ -16,8 +17,22 @@ def detail(request, question_id):
     return render(request, "polls/detail.html", {'question' : question})
 
 def results(request, question_id):
-    response = "Your're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/results.html', {'question' : question })
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question' : question,
+            'error_message' : "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1 # Il peut y avoir une "situation de compétition" entre 2 utilisateurs qui voterait en même temps. Lire "Prévention des conflits de concurrence avec F()"
+        selected_choice.save()
+        # always return an HttpResponseRedirect after successfully dealing with POST data.
+        # This prevents data from being posted twice if a user hits the back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
