2cf5b529c6a84a84001204946d986ab82d1fd4a6
caicos <bruletmarc@yahoo.fr>
2020-12-05 16:27:12 +0100

    Personnalisation du formulaire d'administration - partie 01

--- polls/admin.py
+++ polls/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Question
 
-admin.site.register(Question)
+# Personnalisation de l'affichage et du comportement du formulaire pour le modèle "Question"
+# Cette méthode (de  créer une classe d’administration de modèle, puis le transmettre en tant que deuxième paramètre de admin.site.register())
+# permet de modifier les options d’administration pour un modèle.
+# Le champ « Date de publication » est maintenant affiché avant le champ « Question »
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
