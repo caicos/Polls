64bfb1691b70402e12386c95d7df6e372b1c3c05
caicos <bruletmarc@yahoo.fr>
2020-12-06 23:25:39 +0100

    Personnalisation du formulaire d'administration - partie 04

--- polls/admin.py
+++ polls/admin.py
@@ -2,13 +2,17 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-# Personnalisation de l'affichage et du comportement du formulaire pour le modèle "Question"
-# on partage le formulaire en plusieurs sous-ensembles (fieldsets). (Le premier élément de chaque tuple dans fieldsets est le titre du groupe de champs.)
+# les objets Choice sont édités dans la page d’administration de Question. Par défaut, fournir assez de place pour 3 choix
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
 
