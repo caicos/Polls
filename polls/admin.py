96c470c8cd235039a8d190bbd46955eb7281bbe1
caicos <bruletmarc@yahoo.fr>
2020-12-06 23:09:20 +0100

    Personnalisation du formulaire d'administration - partie 03

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 # Personnalisation de l'affichage et du comportement du formulaire pour le modèle "Question"
 # on partage le formulaire en plusieurs sous-ensembles (fieldsets). (Le premier élément de chaque tuple dans fieldsets est le titre du groupe de champs.)
@@ -11,3 +11,5 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice)
