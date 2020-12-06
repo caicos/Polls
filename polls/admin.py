4abe83e4bb3558db3051dfa18ad2afa2d0608ed2
caicos <bruletmarc@yahoo.fr>
2020-12-07 00:00:49 +0100

    Personnalisation du formulaire d'administration - partie 05

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from .models import Question, Choice
 
 # les objets Choice sont édités dans la page d’administration de Question. Par défaut, fournir assez de place pour 3 choix
-class ChoiceInline(admin.StackedInline):
+# Avec tabular, les objets sont affichés sous forme de tableau
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -13,6 +14,9 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')      # Par défaut, Django affiche le str() de chaque objet. L’option list_display est un tuple de noms de champs particuliers à afficher en colonnes.
+    list_filter = ['pub_date']                                                  # ajoute une barre latérale "Filter" pour filtrer la liste selon le champ "pub_date"
+    search_fields = ['question_text']                                           # ajoute une fonction (boîte) de recherche
 
 admin.site.register(Question, QuestionAdmin)
 
